d9aff7af1fd484e38491cf212052cc479f5c1de0
Ramiro-Mujica <uziel.mujica@davinci.edu.ar>
2025-09-08 15:37:41 -0300

    proyecto

---
+++ super-sano/super-sano-store/src/data/products.js
@@ -0,0 +1,23 @@
+const products = [
+  { id: 1, name: "Harina de Trigo Integral", price: 450, country: "Argentina", image: "./images/harina-trigo-integral.jpg", type: "Harina" },
+  { id: 2, name: "Almendras Naturales", price: 580, country: "España", image: "./images/almendras-naturales.jpg", type: "Frutos secos" },
+  { id: 3, name: "Aceite de Oliva Extra Virgen", price: 720, country: "Italia", image: "./images/aceite-oliva.jpg", type: "Aceite" },
+  { id: 4, name: "Sal del Himalaya", price: 300, country: "Chile", image: "./images/sal-himalaya.jpg", type: "Sal" },
+  { id: 5, name: "Harina de Maíz Amarillo", price: 410, country: "México", image: "./images/harina-maiz.jpg", type: "Harina" },
+  { id: 6, name: "Nueces Pecanas", price: 620, country: "Estados Unidos", image: "./images/nueces-pecanas.jpg", type: "Frutos secos" },
+  { id: 7, name: "Semillas de Chía", price: 280, country: "Argentina", image: "./images/semillas-chia.jpg", type: "Semillas" },
+  { id: 8, name: "Quinoa Blanca", price: 500, country: "Bolivia", image: "./images/quinoa-blanca.jpg", type: "Grano" },
+  { id: 9, name: "Miel Orgánica", price: 350, country: "Argentina", image: "./images/miel-organica.jpg", type: "Endulzante" },
+  { id: 10, name: "Cúrcuma Molida", price: 260, country: "India", image: "./images/curcuma.jpg", type: "Especia" },
+  { id: 11, name: "Harina de Avena", price: 390, country: "Argentina", image: "./images/harina-avena.jpg", type: "Harina" },
+  { id: 12, name: "Pasta Integral", price: 470, country: "Italia", image: "./images/pasta-integral.jpg", type: "Pasta" },
+  { id: 13, name: "Azúcar Mascabo", price: 320, country: "Brasil", image: "./images/azucar-mascabo.jpg", type: "Endulzante" },
+  { id: 14, name: "Semillas de Lino", price: 290, country: "Argentina", image: "./images/semillas-lino.jpg", type: "Semillas" },
+  { id: 15, name: "Aceite de Coco", price: 680, country: "Filipinas", image: "./images/aceite-coco.jpg", type: "Aceite" },
+  { id: 16, name: "Harina de Garbanzo", price: 430, country: "India", image: "./images/harina-garbanzo.jpg", type: "Harina" },
+  { id: 17, name: "Vinagre de Manzana", price: 310, country: "Argentina", image: "./images/vinagre-manzana.jpg", type: "Vinagre" },
+  { id: 18, name: "Té Verde Matcha", price: 540, country: "Japón", image: "./images/te-matcha.jpg", type: "Infusión" },
+  { id: 19, name: "Cacao en Polvo", price: 460, country: "Perú", image: "./images/cacao-polvo.jpg", type: "Chocolate" },
+  { id: 20, name: "Harina de Almendra", price: 720, country: "Estados Unidos", image: "./images/harina-almendra.jpg", type: "Harina" }
+];
+export default products;
